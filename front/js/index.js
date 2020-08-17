6cc516a7aec92dd87ea630dcb5c66593c39ed39b
João Pedro Peres <britoperes@icloud.com>
2020-08-17 12:31:35 -0300

    Inclusão dos arquivos do Bootstrap

---
+++ front/js/index.js
@@ -0,0 +1,8 @@
+// const formulario = document.getElementById('formulario')
+// formulario.addEventListener('submit', e=>{
+//     e.preventDefault()
+// })
+
+const calmaMeuFilho = (evento) =>{
+    evento.preventDefault()
+}
