4f1b4adf66833e6c141290c6e2a8cd793fc96bdb
João Pedro Peres <britoperes@icloud.com>
2020-08-17 20:51:41 -0300

    Criação da função 'getLista'

--- front/js/index.js
+++ front/js/index.js
@@ -3,8 +3,20 @@ const base = 'http://localhost:8080'
 const criaLinhas = (array = [])=>{
     const base = '<tr> <th>#ID</th> <td>#NOME</td> <td>#TELEFONE1</td> <td>#TELEFONE2</td> <td>#ENDERECO</td></tr>'
     const lista = array.reduce((prev, value)=>{
-        return prev + base.replace('#ID', value.id).replace('#NOME',value.nome).replace('#TELEFONE1', value.telefone1).replace('#TELEFONE2', value.telefone2).replace('#ENDERECO', value.endereco)
+        return prev + base.replace('#ID', value.ID).replace('#NOME',value.NOME).replace('#TELEFONE1', value.TELEFONE1).replace('#TELEFONE2', value.TELEFONE2).replace('#ENDERECO', value.ENDERECO)
     }, '')
     const corpoTabela = document.getElementById('corpotabela')
     corpoTabela.innerHTML = lista
 }
+
+const getLista = ()=>{
+    fetch(`${base}/lista`, {method:'GET'}).then(data=>{
+        data.text().then(result=>{
+            const resultJSON = JSON.parse(result)
+            console.log(resultJSON)
+            criaLinhas(resultJSON)
+        })
+    })
+}
+
+getLista()
