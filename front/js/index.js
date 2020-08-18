1c6560808228fc14a3861819f31b9d23b17ec338
João Pedro Peres <britoperes@icloud.com>
2020-08-17 22:20:11 -0300

    Criação das funções 'alteraLista', 'putLista'

--- front/js/index.js
+++ front/js/index.js
@@ -2,9 +2,10 @@ const base = 'http://localhost:8080'
 
 const criaLinhas = (array = [])=>{
     const botaoDelete = '<button onclick = "deleteLista(#ID)" type="button" class="btn btn-danger">deletar</button>'
-    const base = `<tr> <th>#ID</th> <td>#NOME</td> <td>#TELEFONE1</td> <td>#TELEFONE2</td> <td>#ENDERECO</td> <td>${botaoDelete}</td></tr>`
+    const botaoAlterar = '<button onclick = "alterarLista(#ID)"type="button" class="btn btn-warning">Alterar</button>'
+    const base = `<tr id='item#ID'> <th>#ID</th> <td>#NOME</td> <td>#TELEFONE1</td> <td>#TELEFONE2</td> <td>#ENDERECO</td> <td>${botaoDelete}</td><td>${botaoAlterar}</td></tr>`
     const lista = array.reduce((prev, value)=>{
-        return prev + base.replace('#ID', value.ID).replace('#NOME',value.NOME).replace('#TELEFONE1', value.TELEFONE1).replace('#TELEFONE2', value.TELEFONE2).replace('#ENDERECO', value.ENDERECO).replace('#ID', value.ID)
+        return prev + base.replace('#ID', value.ID).replace('#NOME',value.NOME).replace('#TELEFONE1', value.TELEFONE1).replace('#TELEFONE2', value.TELEFONE2).replace('#ENDERECO', value.ENDERECO).replace('#ID', value.ID).replace('#ID', value.ID).replace('#ID', value.ID)
     }, '')
     const corpoTabela = document.getElementById('corpotabela')
     corpoTabela.innerHTML = lista
@@ -51,16 +52,48 @@ const deleteLista = (id)=>{
     })
 }
 
-const limpaCampos = ()=>{ //nao funciona
-    let nome = document.getElementById('nome').textContent
-    nome = ''
+const alterarLista = (id)=>{
+    const item = document.querySelectorAll(`#item${id} td`)
+    document.getElementById('botaoInserir').setAttribute('disabled','true')
+    document.getElementById('botaoAlterar').setAttribute('onclick',`putLista(${id})`)
+    document.getElementById('botaoAlterar').removeAttribute('disabled')
+    document.getElementById('nome').value = item[0].textContent
+    document.getElementById('telefone1').value = item[1].textContent
+    document.getElementById('telefone2').value= item[2].textContent
+    document.getElementById('endereco').value= item[3].textContent
+}
+
+const putLista = (id)=>{
+    const inputNome = document.getElementById('nome')
+    let nome = inputNome.value
     let telefone1 = document.getElementById('telefone1').value
-    telefone1 = ''
     let telefone2 = document.getElementById('telefone2').value
-    telefone2 = ''
     let endereco = document.getElementById('endereco').value
-    endereco =''
-    let inputNome = document.getElementById('nome')
-    inputNome.focus()
+    let myHeaders = new Headers({"Content-Type":"application/json"})
+    const body = {
+        nome,
+        telefone1,
+        telefone2,
+        endereco
+    }
+    console.log(body)
+    console.log(JSON.stringify(body))
+    fetch(`${base}/lista/${id}`,{method:'PUT', body:JSON.stringify(body), headers:myHeaders}).then(data=>{
+        data.text().then(text=>{
+            getLista()
+            limpaCampos()
+            document.getElementById('botaoAlterar').setAttribute('disabled','true')
+            document.getElementById('botaoAlterar').removeAttribute('onclick')
+            document.getElementById('botaoInserir').removeAttribute('disabled')
+        })
+    }).catch(e=>console.error(e))
+}
+
+const limpaCampos = ()=>{ //nao funciona
+    document.getElementById('nome').value = ''
+    document.getElementById('telefone1').value = ''
+    document.getElementById('telefone2').value= ''
+    document.getElementById('endereco').value= ''
+    document.getElementById('nome').focus()
 }
 getLista()
